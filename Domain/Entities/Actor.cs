15f6f68156ed917523d9f0fba87038c2d9b889ab
HarryCopter1 <46008438+HarryCopter1@users.noreply.github.com>
2020-01-09 02:06:37 +0200

    .gitignore is now working

---
+++ Domain/Entities/Actor.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Root;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Entities
+{
+    class Actor : EntityBase
+    {
+        public string First_Name { get; private set; }
+        public string Last_Name { get; private set; }
+    }
+}
---
+++ Domain/Entities/MovieGenre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Domain.Root;
+
+namespace Domain.Entities
+{
+    class MovieGenre : EntityBase
+    {
+        public string Category_ID { get; private set; }
+
+        public string Actor_ID { get; private set; }
+
+        public Genre Genre { get; private set; }
+
+        public Movie Movie { get; private set; }
+    }
+}
